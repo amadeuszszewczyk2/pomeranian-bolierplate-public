babf2d9b286ec24cad3805ab0c56db6ff81e082e
amadeuszszewczyk2 <amadeusz.szewczyk@gmail.com>
2023-07-25 23:08:55 +0200

    db

--- src/App/Exercises/Js/Try6/Try6.jsx
+++ src/App/Exercises/Js/Try6/Try6.jsx
@@ -6,7 +6,7 @@ export function ExerciseTry7() {
   const [error, setError] = useState(null);
 
   const fetchDataFast = () => {
-    return new Promise(resolve =>
+    return new Promise((resolve) =>
       setTimeout(() => {
         resolve({ id: 1, time: 300 });
       }, 300)
@@ -14,7 +14,7 @@ export function ExerciseTry7() {
   };
 
   const fetchDataLong = () => {
-    return new Promise(resolve =>
+    return new Promise((resolve) =>
       setTimeout(() => {
         resolve({ id: 2, time: 700 });
       }, 700)
@@ -31,47 +31,47 @@ export function ExerciseTry7() {
 
   const handleFetchDataFast = () => {
     fetchDataFast()
-      .then(result => setData([result]))
-      .catch(error => setError(error.message));
+      .then((result) => setData([result]))
+      .catch((error) => setError(error.message));
   };
 
   const handleFetchDataLong = () => {
     fetchDataLong()
-      .then(result => setData([result]))
-      .catch(error => setError(error.message));
+      .then((result) => setData([result]))
+      .catch((error) => setError(error.message));
   };
 
   const handleFetchDataError = () => {
     fetchDataError()
-      .then(result => setData([result]))
-      .catch(error => setError(error.message));
+      .then((result) => setData([result]))
+      .catch((error) => setError(error.message));
   };
 
   const handleGetAll = () => {
     Promise.all([fetchDataFast(), fetchDataLong(), fetchDataError()])
-      .then(results => setData(results))
-      .catch(error => setError(error.message));
+      .then((results) => setData(results))
+      .catch((error) => setError(error.message));
   };
 
   const handleGetAllSettled = () => {
     Promise.allSettled([fetchDataFast(), fetchDataLong(), fetchDataError()])
-      .then(results => {
-        const fulfilledResults = results.filter(result => result.status === 'fulfilled');
-        setData(fulfilledResults.map(result => result.value));
+      .then((results) => {
+        const fulfilledResults = results.filter((result) => result.status === 'fulfilled');
+        setData(fulfilledResults.map((result) => result.value));
       })
-      .catch(error => setError(error.message));
+      .catch((error) => setError(error.message));
   };
 
   const handleGetAny = () => {
     Promise.any([fetchDataFast(), fetchDataLong(), fetchDataError()])
-      .then(result => setData([result]))
-      .catch(error => setError(error.message));
+      .then((result) => setData([result]))
+      .catch((error) => setError(error.message));
   };
 
   const handleGetRace = () => {
     Promise.race([fetchDataFast(), fetchDataLong(), fetchDataError()])
-      .then(result => setData([result]))
-      .catch(error => setError(error.message));
+      .then((result) => setData([result]))
+      .catch((error) => setError(error.message));
   };
 
   const handleReset = () => {
@@ -80,16 +80,17 @@ export function ExerciseTry7() {
   };
 
   const handleJsonServerExample = () => {
-    fetch('http://localhost:3000/comments')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => setError(error.message));
+    fetch('http://localhost:3001/comments')
+    // Pamiętać o uruchomieniu bazy danych w terminalu: json-server --watch db.json --port 3001
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error.message));
   };
 
   return (
     <div>
       <button onClick={handleJsonServerExample}>Json Server Example</button>
-       <hr></hr>
+      <hr />
       <button onClick={handleFetchDataFast}>fetchDataFast</button>
       <button onClick={handleFetchDataLong}>fetchDataLong</button>
       <button onClick={handleFetchDataError}>fetchDataError</button>
@@ -98,19 +99,19 @@ export function ExerciseTry7() {
       <button onClick={handleGetAllSettled}>Get All Settled</button>
       <button onClick={handleGetAny}>Get Any</button>
       <button onClick={handleGetRace}>Get Race</button>
-      <hr></hr>
+      <hr />
       <button onClick={handleReset} style={{ backgroundColor: 'red', color: 'white' }}>
         Reset
       </button>
-      <hr></hr>
+      <hr />
       {data && (
         <div>
           {data.map((item, index) => (
             <div key={index}>
               <p>ID: {item.id}</p>
-              <p>Title: {item.title}</p>
-              <p>Author: {item.author}</p>
               <p>Body: {item.body}</p>
+              <p>Post ID: {item.postId}</p>
+              <p>Test: {item.test}</p>
             </div>
           ))}
         </div>
--- src/App/Exercises/Js/Try7/router-data.jsx
+++ src/App/Exercises/Js/Try7/router-data.jsx
@@ -3,7 +3,7 @@ import { ExerciseTry8 } from './Try7'
 export const Try7MetaData = {
   path: 'Try7',
   date: '25.07.2023',
-  linkLabel: 'Promises2',
+  linkLabel: 'Project: Data Base Json-Server',
   blockNo: 35,
   element: <ExerciseTry8 />,
   tags: ['try', 'catch', 'finally', 'javascript'],
