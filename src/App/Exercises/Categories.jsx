cf585809d7714d263a13ea3aca183c502acdd32f
Amadeusz Szewczyk <firma@MacBook-Air-2.local>
2023-09-06 19:14:06 +0200

    icons

--- src/App/Exercises/Categories.jsx
+++ src/App/Exercises/Categories.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import jsIcon from './js.svg';
+import htmlIcon from './html.svg';
+import reactIcon from './react.svg';
+
 import './categories.css';
 
 export const Categories = () => {
   return (
     <>
       <div className="exercises-categories">
-        <h2>Categories</h2>
-
-        <NavLink to="html-css">HTML & CSS</NavLink>
-        <NavLink to="js">Java Script</NavLink>
-        <NavLink to="react">React</NavLink>
-        {/*
-        <NavLink to="web-api">Web API</NavLink>
-        <NavLink to="async">Asynchrony</NavLink>
-        <NavLink to="firebase">Firebase</NavLink> 
-        */}
+        <NavLink
+          to="html-css"
+          className="category-tile"
+          activeClassName="active"
+        >
+          <img src={htmlIcon} alt="HTML icon" />
+          HTML & CSS
+        </NavLink>
+        <NavLink to="js" className="category-tile" activeClassName="active">
+          <img src={jsIcon} alt="JavaScript icon" />
+          Java Script
+        </NavLink>
+        <NavLink to="react" className="category-tile" activeClassName="active">
+          <img src={reactIcon} alt="React icon" />
+          React
+        </NavLink>
       </div>
     </>
   );
