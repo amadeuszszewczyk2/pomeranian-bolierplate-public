9ae884d715ff610b510e0f971a5d06b39f209bf1
Amadeusz Szewczyk <firma@MacBook-Air-2.local>
2023-09-07 20:06:17 +0200

    mobile

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,38 +3,50 @@ import './styles/header.css';
 import { Logo } from '../Components/Logo/Logo';
 import { Link, useNavigate } from 'react-router-dom';
 import profilowe from '../Images/profilowe_prywatne_kolo.png';
+import menuIcon from './menu.svg';
+import logoMobile from './logoMobile.svg';
 
 export function AppHeader() {
   const navigate = useNavigate();
+  const isMobile = window.innerWidth <= 768;
 
   const handleLogout = () => {
     navigate('/login');
   };
 
-  const isMobile = window.innerWidth <= 768;
-
   return (
     <header>
+      {isMobile && <div className="mobile-text">Pomeranian start IT</div>}
+      {isMobile && (
+        <Link to="/dashboard" className="menu-icon-link">
+          <img src={menuIcon} alt="Menu" className="menu-icon-img" />
+        </Link>
+      )}
       <Link to="/dashboard" className="headerlogo">
-        <Logo />
+        {isMobile ? (
+          <img src={logoMobile} alt="Mobile Logo" className="mobilelogo" />
+        ) : (
+          <Logo />
+        )}
       </Link>
-      {!isMobile && (
-        <div className="menu">
-          <div className="user-profile">
-            <Link to="/CV">
-              <img src={profilowe} className="profilowe" alt="" />
-            </Link>
+      <div className="menu">
+        <div className="user-profile">
+          <Link to="/CV">
+            <img src={profilowe} className="profilowe" alt="Profile" />
+          </Link>
+          {!isMobile && (
             <div className="user">
               <p className="user-name">Amadeusz</p>
               <p className="user-position">Szewczyk</p>
             </div>
-          </div>
+          )}
+        </div>
+        {!isMobile && (
           <button className="logout" onClick={handleLogout}>
             Logout
           </button>
-        </div>
-      )}
-      {/* Other code remains unchanged */}
+        )}
+      </div>
     </header>
   );
 }
