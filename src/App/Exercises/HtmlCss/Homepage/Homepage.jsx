a5766cc861fc53c97b5b7a72033fcc51ac076bea
Amadeusz Szewczyk <firma@MacBook-Air-2.lan>
2023-12-04 14:49:46 +0100

    homepage

--- src/App/Exercises/HtmlCss/Homepage/Homepage.jsx
+++ src/App/Exercises/HtmlCss/Homepage/Homepage.jsx
@@ -5,14 +5,6 @@ export function Exercise123() {
   return (
     <div>
       <a
-        href="https://amadeuszs.pomeranian-start-it.pl"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visit my website "Job offers for Ukrainians"
-      </a>
-      <p></p>
-      <a
         href="https://dotacjedlahoreca.pl"
         target="_blank"
         rel="noopener noreferrer"
